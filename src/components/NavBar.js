df4405447824169528bf7cec26d32b65ee1d037f
sahilpatel18 <sahil.41801@gmail.com>
2020-07-13 17:40:02 -0400

    destructuring arguments

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class NavBar extends Component {
-  render() {
-    return (
-      <div>
-        <nav className='navbar navbar-light bg-light '>
-          <a className='navbar-brand' href='/'>
-            Navbar{" "}
-            <span className='badge badge-pill badge-secondary'>
-              {this.props.totalCounters}{" "}
-            </span>
-          </a>
-        </nav>
-      </div>
-    );
-  }
-}
+const NavBar = ({ totalCounters }) => {
+  return (
+    <div>
+      <nav className='navbar navbar-light bg-light '>
+        <a className='navbar-brand' href='/'>
+          Navbar
+          <span className='badge badge-pill badge-secondary m-2'>
+            {totalCounters}
+          </span>
+        </a>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import 'bootstrap/dist/css/bootstrap.css'
-import 'font-awesome/css/font-awesome.css'
-import App from './components/App'
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+import App from "./components/App";
 //import Counters from './components/Counters'
 //import Counter from './components/Counter'
-
 
 ReactDOM.render(<App />, document.querySelector("#root"));
