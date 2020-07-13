ea9eb50d673e517184cca8b9bb44c923f2863568
sahilpatel18 <sahil.41801@gmail.com>
2020-07-13 13:56:39 -0400

    added delete button to end of tr and th

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -14,6 +14,7 @@ export default class Movies extends Component {
             <th>Genre</th>
             <th>Stock</th>
             <th>Rate</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
@@ -23,6 +24,9 @@ export default class Movies extends Component {
               <td>{movie.genre.name}</td>
               <td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button className='btn btn-danger btn-sm'>Delete</button>
+              </td>
             </tr>
           ))}
         </tbody>
