17f0fb65a3c96e54b462e730671f73c283fabb4e
sahilpatel18 <sahil.41801@gmail.com>
2020-07-13 17:30:44 -0400

    lifted state from counters to parent(App). Now sharable state to children via props

--- src/components/Counters.js
+++ src/components/Counters.js
@@ -1,53 +1,23 @@
 import React, { Component } from "react";
 import Counter from "./Counter";
+import "../index.css";
 
 export default class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 0 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-    ],
-  };
-
-  handleDelete = (counterId) => {
-    const counters = this.state.counters.filter((c) => c.id !== counterId);
-    this.setState({ counters });
-  };
-
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-    this.setState({ counters });
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map((c) => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({ counters });
-  };
-
   render() {
     return (
       <div>
         <button
-          onClick={this.handleReset}
+          onClick={this.props.onReset}
           className='btn btn-primary btn-sm m-2'
         >
           Reset
         </button>
-        {this.state.counters.map((counter) => (
+        {this.props.counters.map((counter) => (
           <Counter
             key={counter.id}
-            onDelete={this.handleDelete}
+            onDelete={this.props.onDelete}
             counter={counter}
-            onIncrement={this.handleIncrement}
-            onReset={this.handleReset}
+            onIncrement={this.props.onIncrement}
           />
         ))}
       </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,11 +3,16 @@ import React, { Component } from "react";
 export default class NavBar extends Component {
   render() {
     return (
-      <nav className='navbar navbar-light bg-light'>
-        <a className='navbar-brand'>
-          Navbar
-        </a>
-      </nav>
+      <div>
+        <nav className='navbar navbar-light bg-light '>
+          <a className='navbar-brand' href='/'>
+            Navbar{" "}
+            <span className='badge badge-pill badge-secondary'>
+              {this.props.totalCounters}{" "}
+            </span>
+          </a>
+        </nav>
+      </div>
     );
   }
 }
