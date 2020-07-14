cfa1950ce26a4f1cbc08bf99a078362a459c19a4
sahilpatel18 <sahil.41801@gmail.com>
2020-07-14 11:49:40 -0400

    Like component refactor

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -12,10 +12,13 @@ export default class Movies extends Component {
     this.setState({ movies });
   };
 
-  handleLike = () => {
-    console.log('Like clicked');
-  }
-
+  handleLike = (movie) => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
 
   render() {
     const { length: count } = this.state.movies;
@@ -43,7 +46,10 @@ export default class Movies extends Component {
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
                 <td>
-                  <Like liked={movie.liked} onClick={this.handleLike} />
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
                 </td>
                 <td>
                   <button
--- src/components/common/Like.js
+++ src/components/common/Like.js
@@ -1,16 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Like extends Component {
-  render() {
-    let classes = "fa fa-heart";
-    if (!this.props.liked) classes += "-o";
-    return (
-      <i
-        onClick={this.props.onClick}
-        className={classes}
-        style={{ cursor: "pointer" }}
-        aria-hidden='true'
-      ></i>
-    );
-  }
-}
+const Like = (props) => {
+  let classes = "fa fa-heart";
+  if (!props.liked) classes += "-o";
+  return (
+    <i
+      onClick={props.onClick}
+      className={classes}
+      style={{ cursor: "pointer" }}
+      aria-hidden='true'
+    ></i>
+  );
+};
+
+export default Like;
