557c1a9d322843b72e2ef9deaec027d336058d49
sahilpatel18 <sahil.41801@gmail.com>
2020-07-15 17:01:14 -0400

    handling username and password inputs

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,20 +1,49 @@
 import React, { Component } from "react";
 
 export default class LoginForm extends Component {
+  state = {
+    account: { username: "", password: "" },
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
-        <form action=''>
+        <form onSubmit={this.handleSubmit}>
           <div className='form-group'>
             <label htmlFor='username'>Username</label>
-            <input id='username' type='text' className='form-control' />
+            <input
+              value={account.username}
+              onChange={this.handleChange}
+              id='username'
+              name='username'
+              type='text'
+              className='form-control'
+            />
           </div>
           <div className='form-group'>
             <label htmlFor='password'>Password</label>
-            <input id='password' type='text' className='form-control' />
+            <input
+              id='password'
+              onChange={this.handleChange}
+              type='text'
+              value={account.password}
+              name='password'
+              className='form-control'
+            />
           </div>
-          <button className="btn btn-primary">Login</button>
+          <button className='btn btn-primary'>Login</button>
         </form>
       </div>
     );
