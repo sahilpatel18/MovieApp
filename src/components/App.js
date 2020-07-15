c14578fbffbba65dc991cd57498777dde1bee3eb
sahilpatel18 <sahil.41801@gmail.com>
2020-07-15 15:22:47 -0400

    Linking to movie form

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Customers from "./Customers";
 import Rentals from "./Rentals";
 import NotFound from "./notFound";
 import NavBar from "./Navbar";
+import MovieForm from './MovieForm'
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
       <NavBar />
       <main className='container'>
         <Switch>
+          <Route path='/movies/:id' component={MovieForm} />
           <Route path='/movies' component={Movies} />
           <Route path='/customers' component={Customers} />
           <Route path='/rentals' component={Rentals} />
--- src/components/MoviesTable.js
+++ src/components/MoviesTable.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import Like from "./common/Like";
-
+import {Link} from 'react-router-dom'
 import Table from "./common/Table";
 
 class MoviesTable extends Component {
   columns = [
-    { path: "title", label: "Title" },
+    {
+      path: "title",
+      label: "Title",
+      content: (movie) => (
+        <Link to={`/movies/${movie._id}`}>{movie.title}</Link>
+      ),
+    },
     { path: "genre.name", label: "Genre" },
     { path: "numberInStock", label: "Stock" },
     { path: "dailyRentalRate", label: "Rate" },
