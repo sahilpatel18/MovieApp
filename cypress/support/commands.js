e327a4234669ef3a83725869cfea5ef6f0cca936
sahilpatel18 <sahil.41801@gmail.com>
2020-07-24 18:07:35 -0400

    automate logging in before each test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,15 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add("login", () => {
+  cy.server()
+    .route({
+      method: "POST",
+      url: "/api/auth",
+      status: 200,
+    })
+    .as("validLogin");
+
+  cy.visit("/login");
+  cy.get("input").eq(0).type("sahilpatel18@domain.com");
+  cy.get("input").eq(1).type("abc123");
+  cy.get(".btn").click();
+  cy.wait("@validLogin");
+});
